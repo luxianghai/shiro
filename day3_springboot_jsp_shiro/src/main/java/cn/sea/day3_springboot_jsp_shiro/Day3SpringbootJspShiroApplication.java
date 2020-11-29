39d0bc0cd25e007a42d6e03116973e31ade1a945
luxianghai <3243016771@qq.com>
2020-11-29 22:33:29 +0800

    day3_springboot_jsp_shiro: springboot使用jsp模块整合shiro环境搭建成功

---
+++ day3_springboot_jsp_shiro/src/main/java/cn/sea/day3_springboot_jsp_shiro/Day3SpringbootJspShiroApplication.java
@@ -0,0 +1,13 @@
+package cn.sea.day3_springboot_jsp_shiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3SpringbootJspShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3SpringbootJspShiroApplication.class, args);
+    }
+
+}
---
+++ day3_springboot_jsp_shiro/src/test/java/cn/sea/day3_springboot_jsp_shiro/Day3SpringbootJspShiroApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sea.day3_springboot_jsp_shiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3SpringbootJspShiroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
